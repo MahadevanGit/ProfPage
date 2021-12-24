f257fa07a452607db69b22307048394d15bd0ee1
Mahadevan <rainbow.iad.maha@gmail.com>
2021-12-24 23:56:41 +0800

    Modified-little
    Modified-little

---
+++ assets/js/page-data-json-formatted.js
@@ -0,0 +1,409 @@
+{
+  "prof": {
+    "code": "Prof",
+    "name": "Prof. Suresh V",
+    "gender": "male",
+    "designation": "Associative Professor",
+    "emailId": "email@nus.com",
+    "aboutme": "B.Sc. and M.Sc. Chemistry, Calicut University, India; Ph.D. University of Victoria, BC Canada; Postdoc @ Max-Planck Institute Mainz, Germany, Cornell University, New York, USA.",
+    "pageTitle": "Prof. Sueresh V",
+    "intrestContent": "My research interests are in functional polymers, nanomaterials, exploring the mechanism of nanosafety/nanotoxicity, environmental impact of  microplastics in the environment, materials for environmental applications.",
+    "researchHighLightContent": "My group is interested in synthesizing novel functional molecules, polymers for photovoltaic and environmental applications. The efforts are focused on the design, synthesis, characterization and exploration of applications of materials. In case of  nanomaterials, a range of nanoparticles (e.g. metallic, carbon and polymeric) and their blends with polymers are prepared. Recently, we have established the mechanism of  interaction and toxicity of nanomaterials and microplastics in living cells. Another area of interest involves the development of materials for environmental applications. Both bio(nano)fibers and biomembranes are prepared and tested their efficiency towards water purification.  We are interested  in working closely with industry in the above mentioned areas.",
+    "teachingContent": "<div><ul><li class='verticalList'>CM4252 Polymer Chemistry 2</li><li class='verticalList'>CM5161 Advanced Chemical Laboratory Safety</li></ul></div>",
+    "myphoto": "VALIYAVEETTIL-Suresh.jpg",
+    "facebook": "facebook",
+    "twitter": "twitter",
+    "linkedin": "linkedin"
+  },
+  "underProf": [
+    {
+      "code": "Student",
+      "name": "Gomathi Mahadevan",
+      "gender": "female",
+      "designation": "Ph.D Scholar",
+      "emailId": "email@nus.com",
+      "aboutme": "B.Sc. and M.Sc. Chemistry",
+      "pageTitle": "Gomathi Mahadevan",
+      "intrestContent": "My research interests are in functional polymers, nanomaterials, exploring the mechanism of nanosafety/nanotoxicity, environmental impact of  microplastics in the environment, materials for environmental applications.",
+      "researchHighLightContent": "",
+      "teachingContent": "",
+      "myphoto": "gomathi.jpg"
+    },
+    {
+      "code": "Student",
+      "name": "YJ",
+      "gender": "male",
+      "designation": "Ph.D Scholar",
+      "emailId": "email@nus.com",
+      "aboutme": "B.Sc. and M.Sc. Chemistry",
+      "pageTitle": "YJ",
+      "intrestContent": "My research interests are in functional polymers, nanomaterials, exploring the mechanism of nanosafety/nanotoxicity, environmental impact of  microplastics in the environment, materials for environmental applications.",
+      "researchHighLightContent": "",
+      "teachingContent": "",
+      "myphoto": "YJ.jpg"
+    },
+    {
+      "code": "Staff",
+      "name": "Kathiravan",
+      "gender": "male",
+      "designation": "Research Associative",
+      "emailId": "email@nus.com",
+      "aboutme": "B.Sc.Chemistry",
+      "pageTitle": "Kathiravan",
+      "intrestContent": "My research interests are in functional polymers, nanomaterials, exploring the mechanism of nanosafety/nanotoxicity, environmental impact of  microplastics in the environment, materials for environmental applications.",
+      "researchHighLightContent": "",
+      "teachingContent": "",
+      "myphoto": "Kathiravan.jpg"
+    },
+    {
+      "code": "Alumini",
+      "name": "Sourab",
+      "gender": "male",
+      "designation": "Research Scholar",
+      "emailId": "email@nus.com",
+      "aboutme": "B.Sc.Chemistry",
+      "pageTitle": "Sourab",
+      "intrestContent": "My research interests are in functional polymers, nanomaterials, exploring the mechanism of nanosafety/nanotoxicity, environmental impact of  microplastics in the environment, materials for environmental applications.",
+      "researchHighLightContent": "",
+      "teachingContent": "",
+      "myphoto": "Sourab.jpg"
+    },
+    {
+      "code": "Alumini",
+      "name": "Asma",
+      "gender": "female",
+      "designation": "Ph.D Scholar",
+      "emailId": "email@nus.com",
+      "aboutme": "B.Sc. and M.Sc. Chemistry",
+      "pageTitle": "Asma",
+      "intrestContent": "My research interests are in functional polymers, nanomaterials, exploring the mechanism of nanosafety/nanotoxicity, environmental impact of  microplastics in the environment, materials for environmental applications.",
+      "researchHighLightContent": "",
+      "teachingContent": "",
+      "myphoto": "Asma.jpg"
+    }
+  ],
+  "publications": [
+    {
+      "header": "Journal Papers",
+      "content": [
+        {
+          "year": "2020",
+          "published": [
+            {
+              "order": 1,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2020, 20 (23), 6748."
+            },
+            {
+              "order": 2,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2020, 20 (23), 1234."
+            },
+            {
+              "order": 3,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2020, 20 (23), 3223."
+            },
+            {
+              "order": 4,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2020, 20 (23), 42343."
+            },
+            {
+              "order": 5,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2020, 20 (23), 3423."
+            }
+          ]
+        },
+        {
+          "year": "2019",
+          "published": [
+            {
+              "order": 1,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2019, 20 (23), 6748."
+            },
+            {
+              "order": 2,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2019, 20 (23), 1234."
+            },
+            {
+              "order": 3,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2019, 20 (23), 3223."
+            },
+            {
+              "order": 4,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2019, 20 (23), 42343."
+            },
+            {
+              "order": 5,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2019, 20 (23), 3423."
+            }
+          ]
+        },
+        {
+          "year": "2018",
+          "published": [
+            {
+              "order": 1,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2018, 20 (23), 6748."
+            },
+            {
+              "order": 2,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2018, 20 (23), 1234."
+            },
+            {
+              "order": 3,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2018, 20 (23), 3223."
+            },
+            {
+              "order": 4,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2018, 20 (23), 42343."
+            },
+            {
+              "order": 5,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2018, 20 (23), 3423."
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "header": "Conferences",
+      "content": [
+        {
+          "year": "2020",
+          "published": [
+            {
+              "order": 1,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2020, 20 (23), 6748."
+            },
+            {
+              "order": 2,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2020, 20 (23), 1234."
+            },
+            {
+              "order": 3,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2020, 20 (23), 3223."
+            },
+            {
+              "order": 4,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2020, 20 (23), 42343."
+            },
+            {
+              "order": 5,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2020, 20 (23), 3423."
+            }
+          ]
+        },
+        {
+          "year": "2011",
+          "published": [
+            {
+              "order": 1,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2019, 20 (23), 6748."
+            },
+            {
+              "order": 2,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2019, 20 (23), 1234."
+            },
+            {
+              "order": 3,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2019, 20 (23), 3223."
+            },
+            {
+              "order": 4,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2019, 20 (23), 42343."
+            },
+            {
+              "order": 5,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2019, 20 (23), 3423."
+            }
+          ]
+        },
+        {
+          "year": "2018",
+          "published": [
+            {
+              "order": 1,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2018, 20 (23), 6748."
+            },
+            {
+              "order": 2,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2018, 20 (23), 1234."
+            },
+            {
+              "order": 3,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2018, 20 (23), 3223."
+            },
+            {
+              "order": 4,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2018, 20 (23), 42343."
+            },
+            {
+              "order": 5,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2018, 20 (23), 3423."
+            }
+          ]
+        }
+      ]
+    }
+  ],
+  "serviceAndAwards": [
+    {
+      "header": "Services",
+      "content": [
+        {
+          "year": "2020",
+          "data": [
+            {
+              "order": 1,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2020, 20 (23), 6748."
+            },
+            {
+              "order": 2,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2020, 20 (23), 1234."
+            },
+            {
+              "order": 3,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2020, 20 (23), 3223."
+            },
+            {
+              "order": 4,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2020, 20 (23), 42343."
+            },
+            {
+              "order": 5,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2020, 20 (23), 3423."
+            }
+          ]
+        },
+        {
+          "year": "2019",
+          "data": [
+            {
+              "order": 1,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2019, 20 (23), 6748."
+            },
+            {
+              "order": 2,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2019, 20 (23), 1234."
+            },
+            {
+              "order": 3,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2019, 20 (23), 3223."
+            },
+            {
+              "order": 4,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2019, 20 (23), 42343."
+            },
+            {
+              "order": 5,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2019, 20 (23), 3423."
+            }
+          ]
+        },
+        {
+          "year": "2018",
+          "data": [
+            {
+              "order": 1,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2018, 20 (23), 6748."
+            },
+            {
+              "order": 2,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2018, 20 (23), 1234."
+            },
+            {
+              "order": 3,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2018, 20 (23), 3223."
+            },
+            {
+              "order": 4,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2018, 20 (23), 42343."
+            },
+            {
+              "order": 5,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2018, 20 (23), 3423."
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "header": "Awards",
+      "content": [
+        {
+          "year": "2020",
+          "data": [
+            {
+              "order": 1,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2020, 20 (23), 6748."
+            },
+            {
+              "order": 2,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2020, 20 (23), 1234."
+            },
+            {
+              "order": 3,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2020, 20 (23), 3223."
+            },
+            {
+              "order": 4,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2020, 20 (23), 42343."
+            },
+            {
+              "order": 5,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2020, 20 (23), 3423."
+            }
+          ]
+        },
+        {
+          "year": "2019",
+          "data": [
+            {
+              "order": 1,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2019, 20 (23), 6748."
+            },
+            {
+              "order": 2,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2019, 20 (23), 1234."
+            },
+            {
+              "order": 3,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2019, 20 (23), 3223."
+            },
+            {
+              "order": 4,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2019, 20 (23), 42343."
+            },
+            {
+              "order": 5,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2019, 20 (23), 3423."
+            }
+          ]
+        },
+        {
+          "year": "2018",
+          "data": [
+            {
+              "order": 1,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2018, 20 (23), 6748."
+            },
+            {
+              "order": 2,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2018, 20 (23), 1234."
+            },
+            {
+              "order": 3,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2018, 20 (23), 3223."
+            },
+            {
+              "order": 4,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2018, 20 (23), 42343."
+            },
+            {
+              "order": 5,
+              "text": "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2018, 20 (23), 3423."
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
--- assets/js/page-data.js
+++ assets/js/page-data.js
@@ -3,7 +3,7 @@
 var data ={
     "prof" : {
         "code": "Prof",
-        "name" : "Prof. Suresh V",
+        "name" : "Prof. Suresh Valiyaveettil",
         "gender" : "male",
         "designation" : "Associative Professor",
         "emailId" : "email@nus.com",
@@ -13,9 +13,10 @@ var data ={
         "researchHighLightContent" : "My group is interested in synthesizing novel functional molecules, polymers for photovoltaic and environmental applications. The efforts are focused on the design, synthesis, characterization and exploration of applications of materials. In case of  nanomaterials, a range of nanoparticles (e.g. metallic, carbon and polymeric) and their blends with polymers are prepared. Recently, we have established the mechanism of  interaction and toxicity of nanomaterials and microplastics in living cells. Another area of interest involves the development of materials for environmental applications. Both bio(nano)fibers and biomembranes are prepared and tested their efficiency towards water purification.  We are interested  in working closely with industry in the above mentioned areas.",
         "teachingContent": "<div><ul><li class='verticalList'>CM4252 Polymer Chemistry 2</li><li class='verticalList'>CM5161 Advanced Chemical Laboratory Safety</li></ul></div>",
         "myphoto" : "VALIYAVEETTIL-Suresh.jpg",
-        "facebook":"",
-        "twitter":"",
-        "linkedin":"",
+        "facebook":"facebook link",
+        "twitter":"twitter link",
+        "linkedin":"linkedin link",
+        "phoneNumbers": "+65 12347777 , 0065 6578 1234"
     },
     "underProf":[
         //Student data place here
@@ -138,7 +139,7 @@ var data ={
                     ]
                 },
                 {
-                    "year" : "2019",
+                    "year" : "2011",
                     "published" : [
                         {"order" : 1, "text" : "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2019, 20 (23), 6748."},
                         {"order" : 2, "text" : "X. Zhou, K. Parida, O. Halevi, S. Magdassi, P. S. Lee, Sensors 2019, 20 (23), 1234."},
@@ -231,7 +232,20 @@ var data ={
                 },
             ]
         }
-    ]
+    ],
+    "news":[{
+        "title": "New phd student",
+        "content": "Polymer solar cells (PSCs) have attracted great attention in recent years because of their advantages of easy fabrication, low cost, light weight, and potential for flexible devices. However, the power conversion efficiency (PCE) of the PSCs needs to be improved for future commercial applications. Factors limiting the PCE of the PSCs include the low exploitation of sunlight due to the narrow absorption band of conjugated polymers, and the low charge-transport efficiency in the devices due to the lower charge-carrier mobility of the polymer photovoltaic materials. In this Research News article, recent progress in new conjugated polymer photovoltaic materials fabricated by our group and others is reviewed, including polythiophene (PT) and poly(thienylene vinylene) derivatives with conjugated side chains for a broad absorption band, crosslinked PT derivatives with conjugated bridges for higher hole mobility, and low-bandgap donor–acceptor copolymers for broad, red-shifted absorption to match the solar spectrum.",
+        "news_img" : "Kathiravan.jpg",
+        "footer": "date or some content"
+    },
+    {
+        "title": "New paper puplished by gomathi in chem",
+        "content": "Polymer solar cells (PSCs) have attracted great attention in recent years because of their advantages of easy fabrication, low cost, light weight, and potential for flexible devices. However, the power conversion efficiency (PCE) of the PSCs needs to be improved for future commercial applications. Factors limiting the PCE of the PSCs include the low exploitation of sunlight due to the narrow absorption band of conjugated polymers, and the low charge-transport efficiency in the devices due to the lower charge-carrier mobility of the polymer photovoltaic materials. In this Research News article, recent progress in new conjugated polymer photovoltaic materials fabricated by our group and others is reviewed, including polythiophene (PT) and poly(thienylene vinylene) derivatives with conjugated side chains for a broad absorption band, crosslinked PT derivatives with conjugated bridges for higher hole mobility, and low-bandgap donor–acceptor copolymers for broad, red-shifted absorption to match the solar spectrum.",
+        "news_img" : "gomathi.jpg",
+        "footer": "date or some content"
+    },
+]
 }
 
 
