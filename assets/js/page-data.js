9221f49375f9b89a71f4d12d1baede3362be517a
Mahadevan <rainbow.iad.maha@gmail.com>
2021-12-28 12:55:14 +0800

    modified some content
    modified some content

--- assets/js/page-data.js
+++ assets/js/page-data.js
@@ -15,7 +15,20 @@ var data = {
         "intrestContent": "My research interests are in functional polymers, nanomaterials, exploring the mechanism of nanosafety/nanotoxicity, environmental impact of  microplastics in the environment, materials for environmental applications.",
         "intrestContent_image": "myIntrest.jpg",
         "researchHighLightContent": "My group is interested in synthesizing novel functional molecules, polymers for photovoltaic and environmental applications. The efforts are focused on the design, synthesis, characterization and exploration of applications of materials. In case of  nanomaterials, a range of nanoparticles (e.g. metallic, carbon and polymeric) and their blends with polymers are prepared. Recently, we have established the mechanism of  interaction and toxicity of nanomaterials and microplastics in living cells. Another area of interest involves the development of materials for environmental applications. Both bio(nano)fibers and biomembranes are prepared and tested their efficiency towards water purification.  We are interested  in working closely with industry in the above mentioned areas.",
-        "teachingContent": "<div><ul><li class='verticalList'>CM4252 Polymer Chemistry 2</li><li class='verticalList'>CM5161 Advanced Chemical Laboratory Safety</li></ul></div>",
+        "teachingContent": [
+            {
+                "title": "CM4252 Polymer Chemistry 2",
+                "content": "This module prepares the students for the polymer related industry. It covers the chemistry of polymer degradation under the influence of heat, oxygen and UV light and ways of retardation. The science and technology of elastomers or rubber like polymeric materials will be discussed and compared to solid plastics. The synthesis, properties and applications of contemporary engineering and specialty polymers and the role of additives in plastics will also be discussed.",
+                "image": "polymerChemistry2.jpg",
+                "footer": "date or some content"
+            },
+            {
+                "title": "CM5161 Advanced Chemical Laboratory Safety",
+                "content": "This multidisciplinary module provides a broad coverage on safety-related issues in our laboratories. The main objective of this module is to introduce potential hazards and various safety measures which can be adopted to prevent accidents or personal injuries. Topics such as personal protection, safe handling and disposal of various chemicals, standard operating procedures, risk assessment, emergency measures and first aid practices will be introduced. Legislation and laws pertaining to workplace safety will also be discussed.",
+                "image": "",
+                "footer": "date or some content"
+            }
+        ],
         "myphoto": "VALIYAVEETTIL-Suresh.jpg",
         "facebook": "facebook link",
         "twitter": "twitter link",
@@ -30,10 +43,7 @@ var data = {
             "designation": "Ph.D Scholar",
             "emailId": "email@nus.com",
             "aboutme": "B.Sc. and M.Sc. Chemistry",
-            "pageTitle": "Gomathi Mahadevan",
             "intrestContent": "My research interests are in functional polymers, nanomaterials, exploring the mechanism of nanosafety/nanotoxicity, environmental impact of  microplastics in the environment, materials for environmental applications.",
-            "researchHighLightContent": "",
-            "teachingContent": "",
             "myphoto": "gomathi.jpg"
         },
         {
@@ -43,10 +53,7 @@ var data = {
             "designation": "Ph.D Scholar",
             "emailId": "email@nus.com",
             "aboutme": "B.Sc. and M.Sc. Chemistry",
-            "pageTitle": "YJ",
             "intrestContent": "My research interests are in functional polymers, nanomaterials, exploring the mechanism of nanosafety/nanotoxicity, environmental impact of  microplastics in the environment, materials for environmental applications.",
-            "researchHighLightContent": "",
-            "teachingContent": "",
             "myphoto": "YJ.jpg"
         },
         {
@@ -56,10 +63,7 @@ var data = {
             "designation": "Research Associative",
             "emailId": "email@nus.com",
             "aboutme": "B.Sc.Chemistry",
-            "pageTitle": "Yong Ying Tay",
             "intrestContent": "My research interests are in functional polymers, nanomaterials, exploring the mechanism of nanosafety/nanotoxicity, environmental impact of  microplastics in the environment, materials for environmental applications.",
-            "researchHighLightContent": "",
-            "teachingContent": "",
             "myphoto": "YongYing.jpg"
         },
         {
@@ -69,10 +73,7 @@ var data = {
             "designation": "Research Scholar",
             "emailId": "email@nus.com",
             "aboutme": "B.Sc.Chemistry",
-            "pageTitle": "Sourab",
             "intrestContent": "My research interests are in functional polymers, nanomaterials, exploring the mechanism of nanosafety/nanotoxicity, environmental impact of  microplastics in the environment, materials for environmental applications.",
-            "researchHighLightContent": "",
-            "teachingContent": "",
             "myphoto": "Sourab.jpg"
         },
         {
@@ -81,11 +82,8 @@ var data = {
             "gender": "female",
             "designation": "Ph.D Scholar",
             "emailId": "email@nus.com",
-            "aboutme": "B.Sc. and M.Sc. Chemistry",
-            "pageTitle": "Asma",
+            "aboutme": "I have done B.Sc. and M.Sc. Chemistry",
             "intrestContent": "My research interests are in functional polymers, nanomaterials, exploring the mechanism of nanosafety/nanotoxicity, environmental impact of  microplastics in the environment, materials for environmental applications.",
-            "researchHighLightContent": "",
-            "teachingContent": "",
             "myphoto": "Asma.jpg"
         }
     ],
@@ -237,13 +235,13 @@ var data = {
     "news": [{
         "title": "New phd student",
         "content": "Polymer solar cells (PSCs) have attracted great attention in recent years because of their advantages of easy fabrication, low cost, light weight, and potential for flexible devices. However, the power conversion efficiency (PCE) of the PSCs needs to be improved for future commercial applications. Factors limiting the PCE of the PSCs include the low exploitation of sunlight due to the narrow absorption band of conjugated polymers, and the low charge-transport efficiency in the devices due to the lower charge-carrier mobility of the polymer photovoltaic materials. In this Research News article, recent progress in new conjugated polymer photovoltaic materials fabricated by our group and others is reviewed, including polythiophene (PT) and poly(thienylene vinylene) derivatives with conjugated side chains for a broad absorption band, crosslinked PT derivatives with conjugated bridges for higher hole mobility, and low-bandgap donor–acceptor copolymers for broad, red-shifted absorption to match the solar spectrum.",
-        "news_img": "YongYing.jpg",
+        "image": "",
         "footer": "date or some content"
     },
     {
         "title": "New paper puplished by gomathi in chem",
         "content": "Polymer solar cells (PSCs) have attracted great attention in recent years because of their advantages of easy fabrication, low cost, light weight, and potential for flexible devices. However, the power conversion efficiency (PCE) of the PSCs needs to be improved for future commercial applications. Factors limiting the PCE of the PSCs include the low exploitation of sunlight due to the narrow absorption band of conjugated polymers, and the low charge-transport efficiency in the devices due to the lower charge-carrier mobility of the polymer photovoltaic materials. In this Research News article, recent progress in new conjugated polymer photovoltaic materials fabricated by our group and others is reviewed, including polythiophene (PT) and poly(thienylene vinylene) derivatives with conjugated side chains for a broad absorption band, crosslinked PT derivatives with conjugated bridges for higher hole mobility, and low-bandgap donor–acceptor copolymers for broad, red-shifted absorption to match the solar spectrum.",
-        "news_img": "gomathi.jpg",
+        "image": "gomathi.jpg",
         "footer": "date or some content"
     }
     ],
